22d5e3ad940e4d70b35dcd21349756ac637bb7b0
Matthew Wagaman <averaqedev@gmail.com>
2020-03-17 11:20:10 -0700

    deployment preperation

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -5,12 +5,5 @@ import BrowserRouter from 'react-router-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+  
 })
